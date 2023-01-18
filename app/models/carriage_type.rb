fb23d3b7c49bd8c2b744b920d8849ffa7f19c48b
Pavel Prudilko <79691727+necr0me@users.noreply.github.com>
2023-01-18 14:44:41 +0300

    Update carriage_type.rb

--- app/models/carriage_type.rb
+++ app/models/carriage_type.rb
@@ -5,6 +5,6 @@ class CarriageType < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3, maximum: 32}
   validates :description, length: { maximum: 140 }
-  validates :capacity, presence: true, comparison: { greater_or_equal_to: 0 }
+  validates :capacity, presence: true, comparison: { greater_than_or_equal_to: 0 }
 
 end
