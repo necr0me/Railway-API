45814aae6f90609e26211770a2a5fc30b7a2f8fb
pavel <ortusprogressor@gmail.com>
2023-01-21 22:39:36 +0300

    Covered with tests CarriageAdder and CarriageRemover services

--- spec/services/trains/carriage_adder_service_spec.rb
+++ spec/services/trains/carriage_adder_service_spec.rb
@@ -1,1 +1,81 @@
 require 'rails_helper'
+
+RSpec.describe Trains::CarriageAdderService do
+  let(:train) { create(:train) }
+  let(:carriage) { create(:carriage) }
+
+  subject { described_class.call(train: train, carriage_id: carriage.id) }
+
+  describe '#call' do
+    it 'calls #add_carriage method' do
+      allow_any_instance_of(described_class).to receive(:add_carriage).with(no_args)
+      subject
+    end
+  end
+
+  describe '#add_carriage' do
+    context 'when carriage doesnt exist' do
+      let(:carriage) { build(:carriage) }
+
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is false' do
+        expect(subject.success?).to be_falsey
+      end
+
+      it 'data is nil' do
+        expect(subject.data).to be_nil
+      end
+
+      it 'contains error message that couldnt find carriage without an id' do
+        expect(subject.errors).to include("Couldn't find Carriage without an ID")
+      end
+    end
+
+    context 'when carriage already in train' do
+      let(:carriage) { create(:carriage, train_id: train.id) }
+
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is false' do
+        expect(subject.success?).to be_falsey
+      end
+
+      it 'data is nil' do
+        expect(subject.data).to be_nil
+      end
+
+      it 'contains error message that carriage already in train' do
+        expect(subject.errors).to include('Carriage already in train')
+      end
+    end
+
+    context 'when carriage exist and not in train' do
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is true' do
+        expect(subject.success?).to be_truthy
+      end
+
+      it 'data contains added carriage' do
+        expect(subject.data).to eq(carriage)
+      end
+
+      it 'errors is nil' do
+        expect(subject.errors).to be_nil
+      end
+
+      it 'sets train id to train#id and correct order number' do
+        subject
+        expect(carriage.reload.train_id).to eq(train.id)
+        expect(carriage.order_number).to eq(train.carriages.count)
+      end
+    end
+  end
+end
--- spec/services/trains/carriage_remover_service_spec.rb
+++ spec/services/trains/carriage_remover_service_spec.rb
@@ -1,1 +1,91 @@
 require 'rails_helper'
+
+RSpec.describe Trains::CarriageRemoverService do
+  let(:train) { create(:train) }
+  let(:carriage) { create(:carriage) }
+
+  subject { described_class.call(train: train, carriage_id: carriage.id) }
+
+  describe '#call' do
+    it 'call #remove_carriage method' do
+      allow_any_instance_of(described_class).to receive(:remove_carriage).with(no_args)
+      subject
+    end
+  end
+
+  describe '#remove_carriage' do
+    context 'when carriage doesnt exist' do
+      let(:carriage) { build(:carriage) }
+
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is false' do
+        expect(subject.success?).to be_falsey
+      end
+
+      it 'contains error message that cant find carriage with such id' do
+        expect(subject.errors).to include("Couldn't find Carriage without an ID")
+      end
+    end
+
+    context 'when carriage not in train' do
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is false' do
+        expect(subject.success?).to be_falsey
+      end
+
+      it 'contains error that cant remove carriage that not in train' do
+        expect(subject.errors).to include("Can't remove carriage that not in train")
+      end
+    end
+
+    context 'when carriage in different train' do
+      let(:carriage) { create(:carriage, train_id: create(:train).id) }
+
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is false' do
+        expect(subject.success?).to be_falsey
+      end
+
+      it 'contains error that cant remove carriage from different train' do
+        expect(subject.errors).to include("Can't remove carriage from different train")
+      end
+    end
+
+    context 'when carriage in train' do
+      let(:train) { create(:train, :train_with_carriages) }
+      let(:carriage) { train.carriages.first }
+
+      it 'returns OpenStruct object' do
+        expect(subject).to be_kind_of(OpenStruct)
+      end
+
+      it 'success? value is true' do
+        expect(subject.success?).to be_truthy
+      end
+
+      it 'errors is nil' do
+        expect(subject.errors).to be_nil
+      end
+
+      it 'nullifies train_id and order_number' do
+        subject
+        expect(carriage.reload.order_number).to be_nil
+        expect(carriage.train_id).to be_nil
+      end
+
+      it 'decrements order_numbers of carriages that after removed carriage' do
+        subject
+        expect(train.reload.carriages.pluck(:order_number)).to eq((1..train.carriages.count).to_a)
+      end
+    end
+  end
+end
