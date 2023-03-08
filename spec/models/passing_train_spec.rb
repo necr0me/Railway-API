2b0052ea5294d91120971bc357e49bb8db11f8c6
pavel <ortusprogressor@gmail.com>
2023-03-08 16:58:43 +0300

    Covered with tests PassingTrain scopes

--- spec/models/passing_train_spec.rb
+++ spec/models/passing_train_spec.rb
@@ -17,6 +17,35 @@ RSpec.describe PassingTrain, type: :model do
     end
   end
 
+  describe 'scopes' do
+    let(:time) { DateTime.now }
+
+    before do
+      create(:passing_train, arrival_time: time + 5.minutes, departure_time: time + 10.minutes)
+      create(:passing_train, arrival_time: time - 5.minutes, departure_time: time)
+      create(:passing_train, arrival_time: time.yesterday, departure_time: time.yesterday + 5.minutes)
+    end
+
+    context '#arrives_before' do
+      it 'returns passing trains that arriving before time' do
+        expect(PassingTrain.arrives_before(time).pluck(:arrival_time)).to all(be < time)
+      end
+    end
+
+    context '#arrives_at_the_day' do
+      it 'returns passing trains that arriving only at selected day' do
+        expect(PassingTrain.arrives_at_the_day(time).pluck(:arrival_time))
+          .to all(be_between(time.at_beginning_of_day, time.at_end_of_day))
+      end
+    end
+
+    context '#arrives_after' do
+      it 'returns passing trains that arriving after selected time' do
+        expect(PassingTrain.arrives_after(time).pluck(:arrival_time)).to all(be > time)
+      end
+    end
+  end
+
   describe 'validations' do
     context 'departure time and arrival time' do
       it 'is invalid when departure time > arrival time' do
