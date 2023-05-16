518ef55dbb8199cb814a2d2679c9aeae69d52535
Pavel <ortusprogressor@gmail.com>
2023-05-16 16:10:56 +0300

    Fixed destroy of TrainStop

--- app/models/train_stop.rb
+++ app/models/train_stop.rb
@@ -2,6 +2,15 @@ class TrainStop < ApplicationRecord
   belongs_to :train, inverse_of: :stops
   belongs_to :station, inverse_of: :train_stops
 
+  has_many :arrival_tickets, class_name: "Ticket",
+                             foreign_key: :arrival_stop_id,
+                             inverse_of: :arrival_point,
+                             dependent: :destroy
+  has_many :departure_tickets, class_name: "Ticket",
+                               foreign_key: :departure_stop_id,
+                               inverse_of: :departure_point,
+                               dependent: :destroy
+
   # TODO: add validation of presence (optional)
   validate :arrival_cannot_be_later_than_departure
 
