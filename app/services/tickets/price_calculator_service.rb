c01e7278d6834c2b16cfc0c4af9d19bf8a7abad0
pavel <ortusprogressor@gmail.com>
2023-06-07 15:14:50 +0300

    Added distance factor for ticket price at corresponding service

--- app/services/tickets/price_calculator_service.rb
+++ app/services/tickets/price_calculator_service.rb
@@ -20,8 +20,9 @@ module Tickets
 
     def calculate
       ticket.price = coefficient_for_travel_time * price_by_travel_time
-      ticket.price *= coefficient_for_left_time if Time.now.utc + MAX_LEFT_TIME > ticket.departure_time
-      ticket.price *= UPPER_SEAT_COEFFICIENT if ticket.seat.number.even?
+      ticket.price *= coefficient_for_left_time if out_of_left_time?
+      ticket.price *= coefficient_for_distance
+      ticket.price *= UPPER_SEAT_COEFFICIENT if even_seat?
 
       success!(data: ticket)
     end
@@ -37,6 +38,23 @@ module Tickets
     def coefficient_for_left_time
       1 + [(ticket.departure_time - Time.now.utc) / MAX_LEFT_TIME * LEFT_TIME_MAX_COEFFICIENT,
            LEFT_TIME_MAX_COEFFICIENT].min&.round(2)
+    end
+
+    def coefficient_for_distance
+      order_numbers = ticket.departure_point.train.route.station_order_numbers
+      departure_station_id = ticket.departure_point.station_id
+      arrival_station_id = ticket.arrival_point.station_id
+      arrival_index, departure_index = order_numbers.where(station_id: [arrival_station_id, departure_station_id])
+                                                    .pluck(:order_number)
+      (arrival_index - departure_index).abs / (order_numbers.count - 1).to_f
+    end
+
+    def out_of_left_time?
+      Time.now.utc + MAX_LEFT_TIME > ticket.departure_time
+    end
+
+    def even_seat?
+      ticket.seat.number.even?
     end
   end
 end
