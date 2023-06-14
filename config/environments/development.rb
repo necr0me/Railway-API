bf18aeab432a9fc45d57df61d5560df6f331cd71
Pavel <ortusprogressor@gmail.com>
2023-06-14 12:17:48 +0300

    more mailer config fixes for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :test
-
-  host = Rails.env.production? ? 'railway-api.onrender.com' : 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # SMTP settings for gmail
   # config.action_mailer.smtp_settings = {
