e07e3cc65b75ecec3f36a3082dcd154ac5441912
Pavel <ortusprogressor@gmail.com>
2023-06-14 11:38:07 +0300

    Added production db. Added new check in auth service

--- app/services/auth/authentication_service.rb
+++ app/services/auth/authentication_service.rb
@@ -18,6 +18,7 @@ module Auth
       user ||= User.find_by(unconfirmed_email: email)
       return fail!(error: { email: ["Пользователя с таким email не существует"] }) if user.nil?
       return fail!(error: { email: ["Аккаунт не активирован"] }) unless user.activated?
+      return fail!(error: { email: ["E-mail не подтвержден"] }) if user.activated? && user.unconfirmed_email == email
 
       user.authenticate(password) ? success!(data: user) : fail!(error: { password: ["Неверный пароль"] })
     end
